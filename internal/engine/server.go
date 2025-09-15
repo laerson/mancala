010181e19d855e356e892953ba6b56a24bffda17
laerson <20879937+laerson@users.noreply.github.com>
2025-09-14 23:19:42 -0300

    feat(engine): add move logic

--- internal/engine/server.go
+++ internal/engine/server.go
@@ -24,13 +24,93 @@ func (s *Server) Move(ctx context.Context, req *enginepb.MoveRequest) (*enginepb
 		return errResp(err.Error()), nil
 	}
 
-	// TODO: Implement move logic
-	// The current code only echoes the state back
+	board := req.GetGameState().GetBoard().GetPits()
+	player := req.GetGameState().GetCurrentPlayer()
+	pitIndex := req.GetPitIndex()
+	winner := enginepb.Winner_NO_WINNER
+
+	seeds := board[pitIndex]
+	board[pitIndex] = 0
+
+	// distribution logic
+	for seeds > 0 {
+		pitIndex = (pitIndex + 1) % 14
+		if player == enginepb.Player_PLAYER_ONE && pitIndex == 13 ||
+			player == enginepb.Player_PLAYER_TWO && pitIndex == 6 {
+			pitIndex = (pitIndex + 1) % 14 // Skip opponent's store
+		}
+		board[pitIndex] += 1
+		seeds -= 1
+	}
+	// Check Capture
+	if board[pitIndex] == 1 && board[12-pitIndex] > 0 && isPlayablePit(pitIndex, player) {
+		switch player {
+		case enginepb.Player_PLAYER_ONE:
+			board[6] += 1 + board[12-pitIndex]
+			board[12-pitIndex] = 0
+			board[pitIndex] = 0
+		case enginepb.Player_PLAYER_TWO:
+			board[7] += 1 + board[12-pitIndex]
+			board[12-pitIndex] = 0
+			board[pitIndex] = 0
+		}
+	}
+
+	// Check if the game is finished
+	isFinished := true
+	switch player {
+	case enginepb.Player_PLAYER_ONE:
+		for _, v := range board[:6] {
+			if v > 0 {
+				isFinished = false
+				break
+			}
+		}
+	case enginepb.Player_PLAYER_TWO:
+		for _, v := range board[7:13] {
+			if v > 0 {
+				isFinished = false
+				break
+			}
+		}
+	}
+	if isFinished {
+		// Move remaining seeds to store
+		switch player {
+		case enginepb.Player_PLAYER_ONE:
+			for i, v := range board[7:13] {
+				board[13] += v
+				board[i] = 0
+			}
+		case enginepb.Player_PLAYER_TWO:
+			for i, v := range board[0:6] {
+				board[6] += v
+				board[i] = 0
+			}
+		}
+		// Get Winner
+		if board[6] > board[13] {
+			winner = enginepb.Winner_WINNER_PLAYER_ONE
+		} else if board[13] > board[6] {
+			winner = enginepb.Winner_WINNER_PLAYER_TWO
+		} else {
+			winner = enginepb.Winner_DRAW
+		}
+	}
+	repeatTurn := pitIndex == 6 || pitIndex == 13
+	if !repeatTurn {
+		switch player {
+		case enginepb.Player_PLAYER_ONE:
+			player = enginepb.Player_PLAYER_TWO
+		case enginepb.Player_PLAYER_TWO:
+			player = enginepb.Player_PLAYER_ONE
+		}
+	}
 	next := &enginepb.MoveResult{
-		Board:         req.GetGameState().GetBoard(),
-		CurrentPlayer: req.GetGameState().GetCurrentPlayer(),
-		IsFinished:    false,
-		Winner:        enginepb.Winner_NO_WINNER,
+		Board:         &enginepb.Board{Pits: board},
+		CurrentPlayer: player,
+		IsFinished:    isFinished,
+		Winner:        winner,
 	}
 
 	return &enginepb.MoveResponse{
